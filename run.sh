80e08223a4825640e2ba47614b316105966c8f46
Yankun LI <YLi143@slb.com>
2019-04-01 18:51:52 +0800

    change back to rabbitmq with erlang

--- run.sh
+++ run.sh
@@ -1,69 +1,15 @@
 #!/bin/bash
 echo "This is a test for extension"
+echo 'deb https://packages.erlang-solutions.com/ubuntu trusty contrib' |  sudo tee  /etc/apt/sources.list.d/rabbitmq.list
+echo 'deb http://www.rabbitmq.com/debian/ testing main' |  sudo tee -a /etc/apt/sources.list.d/rabbitmq.list
 
-
-RABBITMQSERVER_VERSION="3.6.16-2"
-
-log ()
-{
-    echo "[$(date +'%F %T.%6N')]: $*"
-}
-
-exit1 ()
-{
-    log "RabbitMQ installation aborted."
-    exit 1
-}
-
-install_package ()
-{
-    local package="$1"
-    echo ""
-    log "Installing package ${package}..."
-    local retry=0
-    local max_retry=3
-    while true ; do
-        ((retry++))
-        sudo apt-get update
-        sudo apt-get install -y ${package}
-        exit_code=$?
-        if [[ $exit_code -eq 0 ]]; then
-            log "Successfully installed package ${package}"
-            break
-        fi
-        log "Last exit code is ${exit_code}"
-        if [[ $retry -le $max_retry ]]; then
-            echo ""
-            log "(${retry}/$max_retry) Wait 5 seconds to retry to install package ${package}"
-            sleep 5
-        else
-            echo ""
-            log "Unable to install package ${package} !"
-			exit1
-            break
-        fi
-    done
-}
-
-install_erlang_rabbitmqserver ()
-{
-    echo ""
-    log "-----------------------------Installing erlang and rabbitmq server-----------------------------"
-    log "Configing hostname resolution"
-    
-    echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" | sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list
-    
-    wget -O - "https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc" | sudo apt-key add -
-    
-    sudo apt-get update
-    
-    install_package "apt-transport-https"
-            
-    install_package "rabbitmq-server=$RABBITMQSERVER_VERSION"
-    
-    install_package "jq"
-    
-    log "-----------------------------Installed erlang and rabbitmq server-----------------------------"
-}
-
-install_erlang_rabbitmqserver
+wget  -O-  https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc| sudo apt-key add -
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc |   sudo apt-key add -
+        
+sudo apt-get update
+        
+        #sudo apt-get install erlang=1:20.3-1
+echo 'y' | sudo apt-get install esl-erlang=1:20.3
+echo 'y','y'| sudo apt-get install rabbitmq-server=3.6.15-1
+echo 'y' | sudo apt-get install jq
+echo 'y' | sudo apt-get install -f
