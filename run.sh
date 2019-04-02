65c1aea87092ebd1801477769014a6a4d80f0a52
Yankun LI <YLi143@slb.com>
2019-04-02 14:24:51 +0800

    add running time into log

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Start to install"
 echo "This is a test for extension"
+date
 echo 'deb https://packages.erlang-solutions.com/ubuntu trusty contrib' |  sudo tee  /etc/apt/sources.list.d/rabbitmq.list
 echo 'deb http://www.rabbitmq.com/debian/ testing main' |  sudo tee -a /etc/apt/sources.list.d/rabbitmq.list
 
@@ -7,9 +9,9 @@ wget  -O-  https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc| su
 wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc |   sudo apt-key add -
         
 sudo apt-get update
-        
-        #sudo apt-get install erlang=1:20.3-1
 echo 'y' | sudo apt-get install esl-erlang=1:20.3
 echo 'y','y'| sudo apt-get install rabbitmq-server=3.6.15-1
 echo 'y' | sudo apt-get install jq
 echo 'y' | sudo apt-get install -f
+date
+echo "End od install"
